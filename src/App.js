0690bedda7a710e930b33bafe2c6dc9e1cda1145
MrGRivera <gabrielrivera2889@gmail.com>
2020-11-27 18:39:17 -0600

    created about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
-  return <div className="App">
-    <h1>Template</h1>
-  </div>;
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutSection from '../components/AboutSection'
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  )
+}
+
+export default AboutUs
