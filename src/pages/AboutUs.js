bc042b678e69e9f877bcbc63af7ab7607c12cc33
MrGRivera <gabrielrivera2889@gmail.com>
2020-11-27 19:17:05 -0600

    created services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import AboutSection from '../components/AboutSection'
-
+import React from "react";
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
-  )
-}
+      <ServicesSection />
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
